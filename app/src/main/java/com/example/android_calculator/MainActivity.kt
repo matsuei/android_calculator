9d6f2187aef34e8742ae51563b11393bb0ad060d
matsuei <matuse.kenta@gmail.com>
2020-07-29 22:00:30 +0900

    数字とクリアボタンの簡易実装

--- app/src/main/java/com/example/android_calculator/MainActivity.kt
+++ app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +17,83 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun sendMessage(view: View) {
-        // Do something in response to button
+    private fun addNumber(inputtedNumber: Int) {
+        val textView = findViewById<TextView>(R.id.resultText)
+        textView.text = buildString {
+            append(textView.text.toString())
+            append(inputtedNumber.toString())
+        }
+    }
+
+    private fun clearResultText() {
+        val textView = findViewById<TextView>(R.id.resultText)
+        textView.text = ""
+    }
+
+    fun onZeroButtonClick(view: View) {
+        addNumber(0)
+    }
+
+    fun onOneButtonClick(view: View) {
+        addNumber(1)
+    }
+
+    fun onTwoButtonClick(view: View) {
+        addNumber(2)
+    }
+
+    fun onThreeButtonClick(view: View) {
+        addNumber(3)
+    }
+
+    fun onFourButtonClick(view: View) {
+        addNumber(4)
+    }
+
+    fun onFiveButtonClick(view: View) {
+        addNumber(5)
+    }
+
+    fun onSixButtonClick(view: View) {
+        addNumber(6)
+    }
+
+    fun onSevenButtonClick(view: View) {
+        addNumber(7)
+    }
+
+    fun onEightButtonClick(view: View) {
+        addNumber(8)
+    }
+
+    fun onNineButtonClick(view: View) {
+        addNumber(9)
+    }
+
+    fun onPlusButtonClick(view: View) {
+    }
+
+    fun onMinusButtonClick(view: View) {
+
+    }
+
+    fun onTimesButtonClick(view: View) {
+
+    }
+
+    fun onDivideButtonClick(view: View) {
+
+    }
+
+    fun onEqualButtonClick(view: View) {
+
+    }
+
+    fun onDotButtonClick(view: View) {
+
+    }
+
+    fun onClearButtonClick(view: View) {
+        clearResultText()
     }
 }
