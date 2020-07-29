85005de287828ca0e4c35d382b105c3f29028964
matsuei <matuse.kenta@gmail.com>
2020-07-29 20:38:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.android_calculator
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+}
