81b50397fa74b50fab4f41df6e07f1c2eced3871
matsuei <matuse.kenta@gmail.com>
2020-08-09 17:00:03 +0900

    基本的な計算はできるようになった

--- app/src/main/java/com/example/android_calculator/MainActivity.kt
+++ app/src/main/java/com/example/android_calculator/MainActivity.kt
@@ -1,37 +1,13 @@
 package com.example.android_calculator
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.Gravity
 import android.view.View
-import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.time.toDuration
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    private fun addNumber(inputtedNumber: Int) {
-        val textView = findViewById<TextView>(R.id.resultText)
-        textView.text = buildString {
-            append(textView.text.toString())
-            append(inputtedNumber.toString())
-        }
-    }
-
-    private fun clearResultText() {
-        val resultTextView = findViewById<TextView>(R.id.resultText)
-        resultTextView.text = ""
-        val symbolTextView = findViewById<TextView>(R.id.symbolText)
-        symbolTextView.text = ""
-    }
-
     enum class Symbol(val text: String) {
         Plus("+"),
         Minus("−"),
@@ -39,9 +15,102 @@ class MainActivity : AppCompatActivity() {
         Divide("÷"),
     }
 
+    private var numberArrayList = arrayListOf<Double>()
+    private var symbolArrayList = arrayListOf<Symbol>()
+    private var recentInputtedNumber: String = ""
+    private var isShowingResult: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun addNumber(inputtedNumber: Int) {
+        val textView = findViewById<TextView>(R.id.resultText)
+        if (isShowingResult) {
+            textView.text = ""
+            isShowingResult = false
+        }
+        textView.text = buildString {
+            append(textView.text.toString())
+            append(inputtedNumber.toString())
+        }
+        this.recentInputtedNumber = buildString {
+            append(recentInputtedNumber)
+            append(inputtedNumber.toString())
+        }
+    }
+
+    private fun clearAll() {
+        val resultTextView = findViewById<TextView>(R.id.resultText)
+        resultTextView.text = ""
+        numberArrayList.clear()
+        symbolArrayList.clear()
+    }
+
     private fun selectSymbol(symbol: Symbol) {
-        val textView = findViewById<TextView>(R.id.symbolText)
-        textView.text = symbol.text
+        if (numberArrayList.count() > 0 && numberArrayList.count() != symbolArrayList.count()) {
+            return
+        }
+        numberArrayList.add(recentInputtedNumber.toDouble())
+        symbolArrayList.add(symbol)
+        val textView = findViewById<TextView>(R.id.resultText)
+        textView.text = buildString {
+            append(textView.text.toString())
+            append(symbol.text)
+        }
+        recentInputtedNumber = ""
+    }
+
+    private fun selectEqual() {
+        if (numberArrayList.count() != symbolArrayList.count()) {
+            return
+        }
+        numberArrayList.add(recentInputtedNumber.toDouble())
+        recentInputtedNumber = ""
+        calculate()
+        val resultText = numberArrayList.first().toString()
+        clearAll()
+        val textView = findViewById<TextView>(R.id.resultText)
+        textView.text = resultText
+        isShowingResult = true
+    }
+
+    private fun calculate() {
+        if (symbolArrayList.isEmpty()) {
+            return
+        }
+        val index = symbolArrayList.indexOfFirst {
+            it == Symbol.Times || it == Symbol.Divide
+        }
+        if (index != -1) {
+            var result = 0.0
+            var symbol = symbolArrayList[index]
+            if (symbol == Symbol.Divide) {
+                result = numberArrayList[index].div(numberArrayList[index + 1])
+            } else if (symbol == Symbol.Times) {
+                result = numberArrayList[index].times(numberArrayList[index + 1])
+            }
+            refreshArray(index,result)
+            calculate()
+            return
+        }
+        var result = 0.0
+        var symbol = symbolArrayList[0]
+        if (symbol == Symbol.Minus) {
+            result = numberArrayList[0].minus(numberArrayList[1])
+        } else if (symbol == Symbol.Plus) {
+            result = numberArrayList[0].plus(numberArrayList[1])
+        }
+        refreshArray(0,result)
+        calculate()
+    }
+
+    private fun refreshArray(index: Int, result: Double) {
+        numberArrayList.removeAt(index)
+        numberArrayList.removeAt(index)
+        numberArrayList.add(index,result)
+        symbolArrayList.removeAt(index)
     }
 
     fun onZeroButtonClick(view: View) {
@@ -101,7 +170,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onEqualButtonClick(view: View) {
-
+        selectEqual()
     }
 
     fun onDotButtonClick(view: View) {
@@ -109,6 +178,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClearButtonClick(view: View) {
-        clearResultText()
+        clearAll()
     }
 }
